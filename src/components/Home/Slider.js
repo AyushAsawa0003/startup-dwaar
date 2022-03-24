63cef37bceb73890b5a12142ce8f162c6da23125
Pranav-Aher <pranav.aher04@gmail.com>
2022-03-24 17:59:32 +0530

    Home Page

---
+++ src/components/Home/Slider.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import Box from './Box';
+
+const Slider = props => {
+    const [ founders, setFounders ] = useState([]);
+
+    const get_data = async () => {
+        let response;
+        if (props.title === "Startup") {
+            response = await fetch('/founder/get_founder/');
+        }else{
+            response = await fetch('/investor/get_investor/');
+        }
+        const result = await response.json();
+        console.log(result);
+        setFounders(result.success);
+    }
+
+    useEffect(() => {
+        get_data();
+    }, [])
+
+    return (
+        <div className='container mt-5'>
+            <div className="d-flex justify-content-between align-items-center">
+                <h5>{props.title}</h5>
+                <p className='mb-0'>View all</p>
+            </div>
+            <div className="d-flex justify-content-between mt-4">
+                {
+                    founders && founders.map( founder => <Box founder={founder} />)
+                }
+                
+                {/* <Box />
+                <Box />
+                <Box />
+                <Box /> */}
+            </div>
+        </div>
+    )
+}
+
+export default Slider;
