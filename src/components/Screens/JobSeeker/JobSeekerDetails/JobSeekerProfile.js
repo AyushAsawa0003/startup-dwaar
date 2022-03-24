677006e2039f9a516a8f0f7806d799e7c5f16372
AyushAsawa0003 <77845546+AyushAsawa0003@users.noreply.github.com>
2022-03-24 18:08:50 +0530

    jobseeker updated

--- src/components/Screens/JobSeeker/JobSeekerDetails/JobSeekerProfile.js
+++ src/components/Screens/JobSeeker/JobSeekerDetails/JobSeekerProfile.js
@@ -1,53 +1,61 @@
 import React from "react";
 
-const JobSeekerProfile = () => {
+const JobSeekerProfile = ({
+  name,
+  title,
+  primarySkill,
+  secondarySkill,
+  location,
+  level,
+}) => {
   return (
-    <div>
-      <div
-        className="card"
+    <div
+      className="card"
+      style={{
+        width: "15rem",
+        flex: "1 1 25%",
+        borderRadius: "10px 10px",
+        boxShadow: "0px 0px 10px 0.2px rgba(0,0,0,0.3)",
+        margin: "20px",
+      }}
+    >
+      <img
+        className="card-img-top"
+        src={`https://randomuser.me/api/portraits/men/${Math.floor(
+          Math.random() * 100
+        )}.jpg`}
+        alt="Card cap"
         style={{
-          width: "15rem",
+          borderRadius: "100%",
+          height: "200px",
+          width: "200px",
+          alignSelf: "center",
+          objectFit: "cover",
+          padding: 10,
+        }}
+      />
+      {/* <hr /> */}
+      <div className="card-body text-center">
+        <h5 className="card-title">{name}</h5>
+        <div className="card-text text-sm">
+          <span>{location}</span> | <span>{level}</span>
+        </div>
+        <div className="card-text text-lg mt">
+          <span>{title}</span>
+        </div>
+        <div className="card-text text-sm">
+          <span>{primarySkill}</span> | <span>{secondarySkill}</span>
+        </div>
+      </div>
 
-          borderRadius: "10px 10px",
-          boxShadow: "0px 0px 10px 0.2px rgba(0,0,0,0.3)",
+      <span
+        className="card-body text-center btn-bg-primary fg-white"
+        style={{
+          borderRadius: "0px 0px 10px 10px ",
         }}
       >
-        <img
-          className="card-img-top"
-          src={require("../../../../assets/img/mentors.jpg")}
-          alt="Card cap"
-          style={{
-            borderRadius: "100%",
-            height: "200px",
-            width: "200px",
-            alignSelf: "center",
-            padding: 10,
-          }}
-        />
-        {/* <hr /> */}
-        <div className="card-body text-center">
-          <h5 className="card-title">Ayush Asawa</h5>
-          <div className="card-text text-sm">
-            <span>Mumbai</span> | <span>Intermediate</span>
-          </div>
-          <div className="card-text text-lg mt">
-            <span>Front End Developer</span>
-          </div>
-          <div className="card-text text-sm">
-            <span>React</span> | <span>Java</span> | <span>Angular</span>
-          </div>
-        </div>
-
-        <span
-          className="card-body text-center btn-bg-primary fg-white"
-          style={{
-            borderRadius: "0px 0px 10px 10px ",
-          }}
-        >
-          {" "}
-          View Profile
-        </span>
-      </div>
+        View Profile
+      </span>
     </div>
   );
 };
