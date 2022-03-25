534e239efbdda29274ed486b427a50260b07c999
AyushAsawa0003 <77845546+AyushAsawa0003@users.noreply.github.com>
2022-03-25 10:28:44 +0530

    changes

--- src/components/Home/Slider.js
+++ src/components/Home/Slider.js
@@ -29,7 +29,7 @@ const Slider = (props) => {
           to={`${props.title.toLowerCase()}/details`}
           style={{ textDecorationLine: "none" }}
         >
-          <p className="mb-0 ">View all</p>
+          <p className="mb-0">View all</p>
         </Link>
       </div>
       <div className="d-flex justify-content-between mt-4">
